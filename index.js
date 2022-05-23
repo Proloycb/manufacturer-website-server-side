d495fdfe36e34e7226ff84f5e65b62bd397bb824
Proloycb <proloychacroborty@gmail.com>
2022-05-23 23:06:37 +0600

    user api created

--- index.js
+++ index.js
@@ -14,12 +14,27 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jttl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-  try{
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unAuthorized Access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+async function run() {
+  try {
     await client.connect();
 
     const partsCollection = client.db('comTechUser').collection('parts');
     const ordersCollection = client.db('comTechUser').collection('orders');
+    const userCollection = client.db('comTechUser').collection('users');
 
     app.get('/parts', async (req, res) => {
       const result = await partsCollection.find().toArray();
@@ -28,10 +43,24 @@ async function run(){
     });
     app.get('/parts/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const part = await partsCollection.findOne(query);
       res.send(part);
     });
+
+    // user create api
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+      res.send({ result, token });
+    })
 
     // orders api
 
@@ -48,21 +77,21 @@ async function run(){
       const filter = { _id: ObjectId(id) }
       const options = { upsert: true };
       const updateDoc = {
-          $set: {
-              availableQuantity: data.updatedQuantity,
-          }
+        $set: {
+          availableQuantity: data.updatedQuantity,
+        }
       }
       const result = await partsCollection.updateOne(
-          filter,
-          updateDoc,
-          options
+        filter,
+        updateDoc,
+        options
       );
       res.send(result);
       console.log(data)
-  })
+    })
 
   }
-  finally{}
+  finally { }
 }
 
 run().catch(console.dir);
