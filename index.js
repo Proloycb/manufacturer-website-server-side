3ba4407035adfebd916223fda2de995407cd22d8
Proloycb <proloychacroborty@gmail.com>
2022-05-26 23:22:35 +0600

    cors policyupdate

--- index.js
+++ index.js
@@ -10,20 +10,6 @@ const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
-
-const corsConfig = {
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("*", cors(corsConfig))
-app.use(express.json())
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
-  next()
-});
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jttl.mongodb.net/?retryWrites=true&w=majority`;
