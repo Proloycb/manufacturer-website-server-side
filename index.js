e349c2ef902dd33166856bcb979cf65d1727720d
Proloycb <proloychacroborty@gmail.com>
2022-05-26 23:30:58 +0600

    cors policyupdate

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const app = express();
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jttl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
